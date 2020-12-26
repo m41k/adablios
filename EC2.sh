303f312f7262d36161c7c20bdbf135cb014dc274
m41k <maik.alberto@hotmail.com>
2020-12-26 14:01:17 -0300

    Create EC2.sh

---
+++ EC2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+MAIN_MENU=('LISTAR' 'CRIAR' 'EXCLUIR' 'MAIN_MENU')
+function menu()
+{
+	X=0
+	Y=15
+	MENU=($@)
+	tput clear
+	tput cup $((X+3)) $Y
+	tput setaf 4
+	tput bold
+	echo "adablios.com"
+	tput sgr0
+	tput cup 5 $((Y+2))
+	tput rev
+	echo "MENU"
+	tput sgr0
+	for (( i=0; i<${#MENU[@]}; i++ ))
+		do
+			tput cup $((i+7)) $Y
+			echo $((i+1)) ${MENU[$i]}
+		done
+	tput cup $((${#MENU[@]}+8)) 15
+	read -p "Escolha uma opção [1-${#MENU[@]}]: " CHOICE
+	tput clear
+	tput sgr0
+	tput rc
+	bash <( wget -qO- https://raw.githubusercontent.com/m41k/adablios/main/${MENU[$((CHOICE-1))]})
+	eval menu "\${${MENU[$((CHOICE-1))]}[@]}"
+	#eval menu "\${${MAIN_MENU[$((CHOICE-1))]}[@]}"
+	#menu eval $var
+}
+menu ${MAIN_MENU[@]}
