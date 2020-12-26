75f0719ea330e49462ff8bbebaaaf75298605632
m41k <maik.alberto@hotmail.com>
2020-12-26 15:29:52 -0300

    Update mmenu.sh

--- mmenu.sh
+++ mmenu.sh
@@ -1,3 +1,7 @@
+#--------------------------------------------------------------------------------#
+#		  PROJETO ADABLIOS - adablios.com				 #
+# 		CREATED BY: maik.alberto@hotmail.com				 #
+#--------------------------------------------------------------------------------#
   wget -qO /tmp/fmenu.sh https://raw.githubusercontent.com/m41k/adablios/main/fmenu.sh
   source /tmp/fmenu.sh
   menu EC2 S3 RDS OUTROS
