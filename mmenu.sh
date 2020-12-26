d5251b01eca28fff0d8552365f7a2b96ce38dcd7
m41k <maik.alberto@hotmail.com>
2020-12-26 15:15:37 -0300

    Update mmenu.sh

--- mmenu.sh
+++ mmenu.sh
@@ -1,3 +1,4 @@
   wget -qO /tmp/fmenu.sh https://raw.githubusercontent.com/m41k/adablios/main/fmenu.sh
   source /tmp/fmenu.sh
   menu EC2 S3 RDS OUTROS
+  bash <( wget -qO- https://raw.githubusercontent.com/m41k/adablios/main/${MENU[$((CHOICE-1))]}.sh)
