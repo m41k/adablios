ae181c315c02f89ed090b1fc0157d5db5f4e090d
m41k <maik.alberto@hotmail.com>
2020-12-26 14:03:15 -0300

    Update EC2.sh

--- EC2.sh
+++ EC2.sh
@@ -27,7 +27,7 @@ function menu()
 	tput sgr0
 	tput rc
 	bash <( wget -qO- https://raw.githubusercontent.com/m41k/adablios/main/${MENU[$((CHOICE-1))]})
-	eval menu "\${${MENU[$((CHOICE-1))]}[@]}"
+	#eval menu "\${${MENU[$((CHOICE-1))]}[@]}"
 	#eval menu "\${${MAIN_MENU[$((CHOICE-1))]}[@]}"
 	#menu eval $var
 }
