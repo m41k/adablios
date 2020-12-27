b8e574006defc0c8b8f61492eab8b706fec3cb9d
m41k <maik.alberto@hotmail.com>
2020-12-27 00:22:46 -0300

    Update EC2.sh

--- EC2.sh
+++ EC2.sh
@@ -6,4 +6,8 @@ source /tmp/fmenu.sh
 menu LISTAR CRIAR_VOLUME EXCLUIR MAIN_MENU
 case $CHOICE in
  1) aws ec2 describe-instances --output table ;;
+ 2) read -p "Zone(us-east-1d): " ZONE
+    read -p "Size(Gb): " SIZE
+    read -p "Type(gp2): " TYPE
+    aws ec2 create-volume --availability-zone $ZONE --size $SIZE --volume-type $TYPE;;
 esac
