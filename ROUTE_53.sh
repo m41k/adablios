a101c87c1df8fee8a33e72b26045ee42c328e914
m41k <maik.alberto@hotmail.com>
2020-12-26 23:52:48 -0300

    Create ROUTE_53.sh

---
+++ ROUTE_53.sh
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------------------#
+#		  PROJETO ADABLIOS - adablios.com				 #
+# 		CREATED BY: maik.alberto@hotmail.com				 #
+#--------------------------------------------------------------------------------#
+source /tmp/fmenu.sh
+menu VERIFICAR DISPONIBILIDADE DOMINIO
+case $CHOICE in
+ 1) 
+  read "Digite o domínio: " DOMAIN
+  aws route53domains check-domain-availability --domain-name $DOMAIN ;;
+esac
