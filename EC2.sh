0a5cb92dc8a782b6ec1b2962c773922046f30b8a
m41k <maik.alberto@hotmail.com>
2020-12-26 22:10:20 -0300

    Update EC2.sh

--- EC2.sh
+++ EC2.sh
@@ -3,7 +3,7 @@
 # 		CREATED BY: maik.alberto@hotmail.com				 #
 #--------------------------------------------------------------------------------#
 source /tmp/fmenu.sh
-menu 'LISTAR' 'CRIAR VOLUME' 'EXCLUIR' 'MAIN MENU-'
+menu "LISTAR" "\"CRIAR VOLUME\"" "EXCLUIR" "\"MAIN MENU\""
 case $CHOICE in
  1) aws ec2 describe-instances --output table ;;
 esac
