4321922d091ccd47900d39faaf664ac7c53063e7
m41k <maik.alberto@hotmail.com>
2020-12-28 11:01:29 -0300

    Update EC2.sh

--- EC2.sh
+++ EC2.sh
@@ -3,11 +3,13 @@
 # 		CREATED BY: maik.alberto@hotmail.com				 #
 #--------------------------------------------------------------------------------#
 source /tmp/fmenu.sh
-menu LISTAR CRIAR_VOLUME EXCLUIR MAIN_MENU
+menu LISTAR_INSTÂNCIAS CRIAR_VOLUME LISTAR_SECURITY_GROUPS MAIN_MENU
 case $CHOICE in
  1) aws ec2 describe-instances --output table ;;
  2) read -p "Zone(us-east-1d): " ZONE
     read -p "Size(Gb): " SIZE
     read -p "Type(gp2): " TYPE
-    aws ec2 create-volume --availability-zone $ZONE --size $SIZE --volume-type $TYPE;;
+    aws ec2 create-volume --availability-zone $ZONE --size $SIZE --volume-type $TYPE ;;
+ 3) aws ec2 describe-secutiry-groups ;;
+ 4) bash <( wget -qO- https://raw.githubusercontent.com/m41k/adablios/main/main.sh) ;;
 esac
