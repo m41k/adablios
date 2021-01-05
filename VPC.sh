2674d735dfb329a6d6280cecaa02b74b00775682
m41k <maik.alberto@hotmail.com>
2021-01-05 00:55:18 -0300

    Update VPC.sh

--- VPC.sh
+++ VPC.sh
@@ -5,10 +5,10 @@
 source /tmp/fmenu.sh
 menu CRIAR_REDE MAIN_MENU
 case $CHOICE in
- 1) read -p "CIDR REDE(192.168.0.0/16): " REDE
+ 1) read -p "CIDR REDE (192.168.0.0/16): " REDE
      VPC=`aws ec2 create-vpc --cidr-block $REDE --query 'Vpc.{VpcId:VpcId}' --output text`
-    read -p "CIDR SUBNET(192.168.1.0/24): " FAIXA
-    read -p "CIDR REDE(us-east-1a): " ZONE
+    read -p "CIDR SUBNET (192.168.1.0/24): " FAIXA
+    read -p "Zona de Disponibilidade (us-east-1a): " ZONE
      SUBNET=`aws ec2 create-subnet --vpc-id $VPC --cidr-block $FAIXA --availability-zone $ZONE --query 'Subnet.{SubnetId:SubnetId}' --output text`
      IGW=`aws ec2 create-internet-gateway --query 'InternetGateway.{InternetGatewayId:InternetGatewayId}' --output text`
      aws ec2 attach-internet-gateway --vpc-id $VPC --internet-gateway-id $IGW
