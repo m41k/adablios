31cca7d2702d5b062c53e0933e9651f332b6abfe
m41k <maik.alberto@hotmail.com>
2021-01-06 01:31:20 -0300

    Update COST_EXPLORER.sh

--- COST_EXPLORER.sh
+++ COST_EXPLORER.sh
@@ -5,8 +5,8 @@
 source /tmp/fmenu.sh
 menu GET_COST MAIN_MENU
 case $CHOICE in
- 1) read -p "DATA INICIO (AAAA/MM/DD): " START
-    read -p "DATA FINAL (AAAA/MM/DD): " END
+ 1) read -p "DATA INICIO (AAAA-MM-DD): " START
+    read -p "DATA FINAL  (AAAA-MM-DD): " END
  aws ce get-cost-and-usage --time-period Start=$START,End=$END --granularity MONTHLY --metrics "BlendedCost"
  ;;
 esac
